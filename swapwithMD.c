f260ec58ddae2a0835612d88d8d1a755c3879e85
shyamsujith <24211a05ev@bvrit.ac.in>
2025-07-27 17:05:54 +0530

    Add files via upload

---
+++ swapwithMD.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b;
+
+	printf("\nenter the values of a and b:");
+	scanf("%d%d",&a,&b);
+	printf("\nbefore swapping a=%d\tb=%d",a,b);
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	printf("\nbefore swapping a=%d\tb=%d",a,b);
+	return 0;
+}
